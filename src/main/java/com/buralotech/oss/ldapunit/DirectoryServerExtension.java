905bc690cb104b83e3128eb72ff74ae83cc5f3a1
bmatthews68 <bmatthews68@gmail.com>
2025-07-24 17:10:15 +0300

    Updated copyright headers

--- src/main/java/com/buralotech/oss/ldapunit/DirectoryServerExtension.java
+++ src/main/java/com/buralotech/oss/ldapunit/DirectoryServerExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2024 Brian Thomas Matthews
+ * Copyright 2021-2025 Brian Thomas Matthews
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
